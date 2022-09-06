3acce13a707ca832ddb833a8f9d2d216d555f883
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-09-05 23:31:13 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { GlobalStyle } from "./styles/global";
 export function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>DT Money</h1>
       <GlobalStyle />
     </div>
   );
