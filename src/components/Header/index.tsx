4a0013835bb5e8ac34f9652ca48325a27d63d02e
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-09-11 19:13:16 -0300

    feat: start header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,16 @@
+import logoImg from "../../assets/logo.svg"
+
+import { Container, Content } from "./styles"
+
 export function Header() {
-  return <header>
-    <img src="" alt="dt money" />
-    <button type="button">
-      Nova transação
-    </button>
-  </header>
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button">
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  )
 }
